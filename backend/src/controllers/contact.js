a8af3e021b74bc084ab53b7057468d46ca4d5454
codemasternode <marcinwarzybok@outlook.com>
2019-10-08 00:15:09 +0200

    search working properly

--- backend/src/controllers/contact.js
+++ backend/src/controllers/contact.js
@@ -39,3 +39,65 @@ export async function contactToUs(req, res) {
     });
   res.send({});
 }
+
+export async function contactToCompany(req, res) {
+  const requireContactProperties = [
+    "name",
+    "surname",
+    "email",
+    "phone",
+    "taxNumber",
+    "description",
+    "points",
+    "companyEmail"
+  ];
+
+  for (let i = 0; i < requireContactProperties.length; i++) {
+    if (!req.body[requireContactProperties[i]]) {
+      return res.status(400).send({
+        msg: `Missing ${requireContactProperties[i]} property`
+      });
+    }
+  }
+
+  const {
+    name,
+    surname,
+    email,
+    phone,
+    taxNumber,
+    description,
+    companyEmail,
+    points
+  } = req.body;
+
+  let generatedURL = "https://www.google.com/maps/dir/?api=1&";
+  for (let i = 0; i < points.length; i++) {
+    if (i === 0) {
+      generatedURL += `origin=${points[i].lat},${points[i].lng}&waypoints=`;
+    } else if (i === points.length - 1) {
+      generatedURL = generatedURL.substring(0, generatedURL.length - 1);
+      generatedURL += `&destination=${points[i].lat},${points[i].lng}`;
+    } else {
+      generatedURL += `${points[i].lat},${points[i].lng}|`;
+    }
+  }
+  generatedURL += "&travelmode=driving"
+  console.log(generatedURL);
+
+  // loadTemplate("contactToCompany", [
+  //   { name, surname, email, phone, taxNumber, description, companyEmail }
+  // ])
+  //   .then(result => {
+  //     sendEmail({
+  //       to: "marcinwarzybok@outlook.com",
+  //       subject: `New Customer ${name} ${surname}`,
+  //       html: result[0].email.html,
+  //       text: result[0].email.text
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //   });
+  res.send({});
+}
--- backend/src/routes/contact.js
+++ backend/src/routes/contact.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { contactToUs } from "../controllers/contact";
+import { contactToUs, contactToCompany } from "../controllers/contact";
 
 const router = express.Router();
 
 export default () => {
   router.post("/contact-to-us", contactToUs);
+  router.post("/contact-to-company", contactToCompany);
   return router;
 };
