dae978171915ac0775696e5bbbf2fb7e36b78b2a
xarchie25 <gartur25@o2.pl>
2019-11-27 17:40:02 +0100

    Request and display data

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./index.scss";
+var __html = require('../../home/index.html.js');
+var template = { __html: __html };
 
 function Home() {
-    return <h1>Home</h1>;
+    return (
+        <div dangerouslySetInnerHTML={template} />
+    );
 }
 
 export default Home;
