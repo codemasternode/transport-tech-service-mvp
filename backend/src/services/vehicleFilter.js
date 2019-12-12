015b4190bd3379cfec147307a9df485d8917749e
codemasternode <marcinwarzybok@outlook.com>
2019-12-12 14:38:33 +0100

    repair vehicle filter

--- backend/src/services/vehicleFilter.js
+++ backend/src/services/vehicleFilter.js
@@ -9,32 +9,41 @@ function costVolume(truck, volume, capacity) {
 }
 
 export function vehicleFilterByPallet(trucks, palettes, weight) {
-    var w = weight / palettes;
-    var best_path = [{ cost: 0 }];
-    trucks.forEach(function (truck) {
-        var max_palettes = Math.min(truck.palettes, truck.capacity / w);
+    var w = weight/palettes;
 
-        for (var i = max_palettes - 1; 0 <= i; i--) {
-            for (var j = 1; j <= Math.min(max_palettes, palettes - i); j++) {
+    // To begin, we can carry nothing for no weight.
+    var best_path = [{cost: 0}];
+    trucks.forEach(function (truck) {
+        var max_pallets = Math.floor((Math.min(truck.palettes, truck.capacity/w)));
+        // i is the number of pallets other trucks carry.
+        // We count down so that there is no chance the solution there
+        // has already used this truck.
+        for (var i = max_pallets-1; 0 <= i; i--) {
+            // j is the number of pallets that truck carries
+            for (var j = 1; j <= Math.min(max_pallets, palettes - i); j++) {
+                // Do we improve on i+j pallets by having truck carry i?
                 if (best_path[i] == null) {
+                    // Other trucks can't carry i
                     continue;
                 }
                 let prev_node = best_path[i];
                 let this_solution = {
                     truck: truck,
                     palettes: j,
-                    cost: prev_node.cost + costPallet(truck, j, w * j),
+                    cost: prev_node.cost + costPallet(truck, j, w*j),
                     prev_node: prev_node
                 };
-                if (best_path[i + j] == null) {
-                    best_path[i + j] = this_solution;
+                if (best_path[i+j] == null) {
+                    best_path[i+j] = this_solution;
                 }
-                else if (this_solution.cost < best_path[i + j].cost) {
-                    best_path[i + j] = this_solution;
+                else if (this_solution.cost < best_path[i+j].cost) {
+                    best_path[i+j] = this_solution;
                 }
             }
         }
     });
+
+    // The answer is a linked list.  Let's decode it for convenience.
     if (best_path[palettes] == null) {
         return null;
     }
@@ -53,13 +62,11 @@ export function vehicleFilterByPallet(trucks, palettes, weight) {
 }
 
 export function vehicleFilterByVolume(trucks, volume, weight) {
-    volume = Math.floor(volume * 10)
+    volume = Math.ceil(volume)
     var w = weight / volume;
     // To begin, we can carry nothing for no weight.
-    console.log(trucks)
     var best_path = [{ cost: 0 }];
     trucks.forEach(function (truck) {
-        truck.volume = Math.floor(truck.volume * 10)
         var max_volume = Math.floor((Math.min(truck.volume, truck.capacity / w)));
         // i is the number of volume other trucks carry.
         // We count down so that there is no chance the solution there
@@ -106,3 +113,4 @@ export function vehicleFilterByVolume(trucks, volume, weight) {
         return answer;
     }
 }
+
