904bb17264efa7cdfb5741169cb3e4314310529d
codemasternode <marcinwarzybok@outlook.com>
2019-12-19 11:19:05 +0100

    prepare for ec2 instance

--- backend/dist/enums/planTypes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports["default"] = void 0;
-var _default = ["Basic", "Pro", "Enterprise"];
-exports["default"] = _default;
-//# sourceMappingURL=planTypes.js.map
